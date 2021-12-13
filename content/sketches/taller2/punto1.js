e442cf0942129505c0389577b810c7eb03bc1c29
Rafael Ruiz <rafael.ruiz@valid.com>
2021-12-12 22:34:44 -0500

    Implementaciones del código en p5 funcionales

--- content/sketches/taller2/punto1.js
+++ content/sketches/taller2/punto1.js
@@ -1,1 +1,163 @@
-Place Ray code here
+class Boundary {
+  constructor(x1, y1, x2, y2) {
+    this.a = createVector(x1, y1);
+    this.b = createVector(x2, y2);
+  }
+
+  show() {
+    stroke(255);
+    line(this.a.x, this.a.y, this.b.x, this.b.y);
+  }
+}
+
+
+class Particle {
+  constructor() {
+    this.pos = createVector(width / 2, height / 2);
+    this.rays = [];
+    for (let a = 0; a < 360; a += 1) {
+      this.rays.push(new Ray(this.pos, radians(a)));
+    }
+  }
+
+  update(x, y) {
+    this.pos.set(x, y);
+  }
+
+  look(walls) {
+    for (let i = 0; i < this.rays.length; i++) {
+      const ray = this.rays[i];
+      let closest = null;
+      let record = Infinity;
+      for (let wall of walls) {
+        const pt = ray.cast(wall);
+        if (pt) {
+          const d = p5.Vector.dist(this.pos, pt);
+          if (d < record) {
+            record = d;
+            closest = pt;
+          }
+        }
+      }
+      if (closest) {
+        // colorMode(HSB);
+        // stroke((i + frameCount * 2) % 360, 255, 255, 50);
+        stroke(255, 100);
+        line(this.pos.x, this.pos.y, closest.x, closest.y);
+      }
+    }
+  }
+
+  show() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 4);
+    for (let ray of this.rays) {
+      ray.show();
+    }
+  }
+}
+
+class Ray {
+  constructor(pos, angle) {
+    this.pos = pos;
+    this.dir = p5.Vector.fromAngle(angle);
+  }
+
+  lookAt(x, y) {
+    this.dir.x = x - this.pos.x;
+    this.dir.y = y - this.pos.y;
+    this.dir.normalize();
+  }
+
+  show() {
+    stroke(255);
+    push();
+    translate(this.pos.x, this.pos.y);
+    line(0, 0, this.dir.x * 10, this.dir.y * 10);
+    pop();
+  }
+
+  cast(wall) {
+    const x1 = wall.a.x;
+    const y1 = wall.a.y;
+    const x2 = wall.b.x;
+    const y2 = wall.b.y;
+
+    const x3 = this.pos.x;
+    const y3 = this.pos.y;
+    const x4 = this.pos.x + this.dir.x;
+    const y4 = this.pos.y + this.dir.y;
+
+    const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+    if (den == 0) {
+      return;
+    }
+
+    const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+    const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+    if (t > 0 && t < 1 && u > 0) {
+      const pt = createVector();
+      pt.x = x1 + t * (x2 - x1);
+      pt.y = y1 + t * (y2 - y1);
+      return pt;
+    } else {
+      return;
+    }
+  }
+}
+
+// 2D Ray Casting
+
+let walls = [];
+let ray;
+let particle;
+let xoff = 0;
+let yoff = 10000;
+
+function setup() {
+  createCanvas(400, 400);
+  /*for (let i = 0; i < 10; i++) {
+    let x1 = random(width);
+    let x2 = random(width);
+    let y1 = random(height);
+    let y2 = random(height);
+    walls[i] = new Boundary(x1, y1, x2, y2);
+  }*/
+  walls.push(new Boundary(10, 10, 390, 10));
+  walls.push(new Boundary(390, 10, 390, 162));
+  walls.push(new Boundary(390, 238, 390, 390));
+  walls.push(new Boundary(10, 390, 390, 390));
+  walls.push(new Boundary(10, 86, 10, 390));
+  walls.push(new Boundary(10, 162, 162, 162));
+  walls.push(new Boundary(238, 162, 390, 162));
+  walls.push(new Boundary(86, 10, 86, 86));
+  walls.push(new Boundary(86, 238, 86, 314));
+  walls.push(new Boundary(162, 162, 162, 86));
+  walls.push(new Boundary(314, 162, 314, 86));
+  walls.push(new Boundary(238, 162, 238, 238));
+  walls.push(new Boundary(314, 238, 314, 314));
+  walls.push(new Boundary(162, 86, 238, 86));
+  walls.push(new Boundary(86, 238, 238, 238));
+  walls.push(new Boundary(314, 238, 390, 238));
+  walls.push(new Boundary(162, 314, 314, 314));
+
+  /*walls.push(new Boundary(width, -1, width, height));
+  walls.push(new Boundary(width, height, -1, height));
+  walls.push(new Boundary(-1, height, -1, -1));*/
+  particle = new Particle();
+}
+
+function draw() {
+  background(50);
+  for (let wall of walls) {
+    wall.show();
+  }
+  //particle.update(noise(xoff) * width, noise(yoff) * height);
+  particle.update(mouseX, mouseY);
+  particle.show();
+  particle.look(walls);
+
+  xoff += 0.01;
+  yoff += 0.01;
+}
+
